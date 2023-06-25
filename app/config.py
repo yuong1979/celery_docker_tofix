1998b0a5bf60461f084762da8a6a7c31cb979937
Ong Yu <yuong1979@gmail.com>
2023-06-26 00:59:56 +0800

    cleaned up

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    DEBUG = True
+    DEBUG = False
     SECRET_KEY = 'very_very_secure_and_secret'
 
     if DEBUG:
@@ -15,10 +15,3 @@ class Config:
         ### redis #### 
         CELERY_BROKER_URL = os.getenv("CELERY_BROKER", 'redis://localhost:6379/0')
         CELERY_RESULT_BACKEND = os.getenv("CELERY_BACKEND", 'redis://localhost:6379/0')
-
-
-    # buy pendle and eth (growing extremely rapidly) and a very very large market potential in interest rate swaps
-    # buy some frax and leave it in convex (they are only 0.2B behind uniswap and only deal with stable swaps - so they are still the king) - https://youtu.be/4A_-_gkPC14?t=189
-    # if you think that curve has potential consider concex owning 50% of curve and than frax owning 7% of convex
-
-
